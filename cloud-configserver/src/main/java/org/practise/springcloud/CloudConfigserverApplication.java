12cb4213f7aed6c9d7561d6ddcd2faed56c6a9c3
Venkat5357 <venkat5357@gmail.com>
2017-07-12 22:33:55 -0500

    First commit

---
+++ cloud-configserver/src/main/java/org/practise/springcloud/CloudConfigserverApplication.java
@@ -0,0 +1,14 @@
+package org.practise.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigserverApplication.class, args);
+	}
+}
